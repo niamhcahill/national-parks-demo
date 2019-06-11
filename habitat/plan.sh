1e14ed468d9667c6c5cba244af0c32e953def96e
Niamh <39315555+niamhcahill@users.noreply.github.com>
2019-06-10 18:10:28 -0700

    Updated version for demo

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=nc-np
-pkg_version=7.0.5
+pkg_version=7.0.6
 pkg_maintainer="Niamh Cahill"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
