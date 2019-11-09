eeecc0c8b9352c671feb4cfc0755ce31eacada14
niamhcahill <ncahill@chef.io>
2019-11-08 16:56:44 -0800

    Update with Jeff Vogt's AKS Changes

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=nc-np
-pkg_version=7.0.6
+pkg_version=7.0.9
 pkg_maintainer="Niamh Cahill"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/corretto core/mongo-tools)
